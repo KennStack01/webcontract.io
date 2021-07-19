620bd94010a049ee7edee1177b7bc677d067f940
Kenn Stack <kibadikennedy@gmail.com>
2021-07-20 01:11:52 +0800

    added contact form functionalities

--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -4,7 +4,7 @@ const Service = ({title, details}) => {
     return (
         <div className="shadow rounded mx-6 my-8 md:mx-20 p-5 hover:shadow-lg">
             <h1 className="text-xl font-semibold my-3"> {title} </h1>
-            <p className="text-md my-6 text-justify"> {details} </p>
+            <p className="text-md font-normal my-6 text-justify"> {details} </p>
         </div>
     )
 }
